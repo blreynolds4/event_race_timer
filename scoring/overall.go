a43fed09292a9cd6601419c94b10e15a6eaec647
Matt Reynolds <32142979+MattReyn@users.noreply.github.com>
2023-08-09 18:44:13 -0400

    test passes

--- scoring/overall.go
+++ scoring/overall.go
@@ -3,7 +3,6 @@ package scoring
 import (
 	"blreynolds4/event-race-timer/competitors"
 	"blreynolds4/event-race-timer/events"
-	"fmt"
 	"time"
 )
 
@@ -27,31 +26,28 @@ type overallScorer struct {
 }
 
 func (os *overallScorer) Score(inputEvents events.EventSource, athletes competitors.CompetitorLookup) ([]ScoredResult, error) {
-	//start := []events.StartEvent{}
-	//finishes := map[int]events.FinishEvent{} //key is bib number
-	places := map[int]events.PlaceEvent{} //key is place
+	start := []events.StartEvent{}
+	finishes := map[int]events.FinishEvent{} //key is bib number
+	places := map[int]events.PlaceEvent{}    //key is place
 	result := []ScoredResult{}
 
-	for event, err := inputEvents.GetRaceEvent(); event != nil && err == nil; {
-		fmt.Println("read event", event)
-		// switch event.GetType() {
-		// case events.StartEventType:
-		// 	start = append(start, event.(events.StartEvent))
-		// case events.FinishEventType:
-		// 	finishes[event.(events.FinishEvent).GetBib()] = event.(events.FinishEvent)
-		// case events.PlaceEventType:
-		// 	places[event.(events.PlaceEvent).GetPlace()] = event.(events.PlaceEvent)
-		// }
+	event, err := inputEvents.GetRaceEvent()
 
+	for event != nil && err == nil {
+		switch event.GetType() {
+		case events.StartEventType:
+			start = append(start, event.(events.StartEvent))
+		case events.FinishEventType:
+			finishes[event.(events.FinishEvent).GetBib()] = event.(events.FinishEvent)
+		case events.PlaceEventType:
+			places[event.(events.PlaceEvent).GetPlace()] = event.(events.PlaceEvent)
+		}
+
+		event, err = inputEvents.GetRaceEvent()
 	}
 
-	//find first place
-	//fill in info for a scored result and append to result
-	//remove from arrays
-	//repeat with next postion
-
 	for place, event := range places {
-		result = append(result, ScoredResult{athletes[event.GetBib()], place, time.Duration(1)})
+		result = append(result, ScoredResult{athletes[event.GetBib()], place, finishes[event.GetBib()].GetFinishTime().Sub(start[0].GetStartTime())})
 	}
 
 	return result, nil

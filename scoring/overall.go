d89cbbee0737813d0f051a83de194de255f9b06a
Matt Reynolds <32142979+MattReyn@users.noreply.github.com>
2023-08-10 10:06:05 -0400

    test passed

--- scoring/overall.go
+++ scoring/overall.go
@@ -26,5 +26,29 @@ type overallScorer struct {
 }
 
 func (os *overallScorer) Score(inputEvents events.EventSource, athletes competitors.CompetitorLookup) ([]ScoredResult, error) {
-	return []ScoredResult{}, nil
+	start := []events.StartEvent{}
+	finishes := map[int]events.FinishEvent{} //key is bib number
+	places := map[int]events.PlaceEvent{}    //key is place
+	result := []ScoredResult{}
+
+	event, err := inputEvents.GetRaceEvent()
+
+	for event != nil && err == nil {
+		switch event.GetType() {
+		case events.StartEventType:
+			start = append(start, event.(events.StartEvent))
+		case events.FinishEventType:
+			finishes[event.(events.FinishEvent).GetBib()] = event.(events.FinishEvent)
+		case events.PlaceEventType:
+			places[event.(events.PlaceEvent).GetPlace()] = event.(events.PlaceEvent)
+		}
+
+		event, err = inputEvents.GetRaceEvent()
+	}
+
+	for place, event := range places {
+		result = append(result, ScoredResult{athletes[event.GetBib()], place, finishes[event.GetBib()].GetFinishTime().Sub(start[0].GetStartTime())})
+	}
+
+	return result, nil
 }

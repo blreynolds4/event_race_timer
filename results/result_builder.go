c2a960fe22118e2a3302d634f3f0f01a42c4f545
Matt Reynolds <32142979+MattReyn@users.noreply.github.com>
2023-08-25 14:24:57 -0400

    restructured, no order required

--- results/result_builder.go
+++ results/result_builder.go
@@ -3,6 +3,8 @@ package results
 import (
 	"blreynolds4/event-race-timer/competitors"
 	"blreynolds4/event-race-timer/events"
+	"fmt"
+	"time"
 )
 
 type ResultBuilder interface {
@@ -21,36 +23,65 @@ type resultBuilder struct {
 
 func (os *resultBuilder) BuildResults(inputEvents events.EventSource, athletes competitors.CompetitorLookup, results ResultTarget) error {
 	start := []events.StartEvent{}
-	finishes := map[int]events.FinishEvent{} //key is bib number
-	places := []events.PlaceEvent{}
 
-	incomplete := []events.PlaceEvent{}
+	rr := map[int]RaceResult{}
+	ft := map[int]time.Time{}
 
 	event, err := inputEvents.GetRaceEvent()
 
 	for event != nil && err == nil {
 		switch event.GetType() {
 		case events.StartEventType:
+			fmt.Println("start event")
 			start = append(start, event.(events.StartEvent))
+
+			for _, result := range rr {
+				result.Time = ft[result.Bib].Sub(start[0].GetStartTime())
+				rr[result.Bib] = result
+
+				if rr[result.Bib].IsComplete() {
+					fmt.Println("send result")
+					results.SendResult(rr[result.Bib])
+				}
+			}
 		case events.FinishEventType:
-			finishes[event.(events.FinishEvent).GetBib()] = event.(events.FinishEvent)
+			fe := event.(events.FinishEvent)
+
+			result := rr[fe.GetBib()]
+			result.Bib = fe.GetBib()
+			result.Athlete = athletes[fe.GetBib()]
+			fmt.Println("finishEvent2")
+			if len(start) > 0 {
+				fmt.Println("finishEvent2.1")
+				result.Time = fe.GetFinishTime().Sub(start[0].GetStartTime())
+			} else {
+				fmt.Println("finishEvent2.2")
+				ft[fe.GetBib()] = fe.GetFinishTime()
+			}
+
+			fmt.Println("finishEvent3")
+			rr[fe.GetBib()] = result
+
+			if rr[fe.GetBib()].IsComplete() {
+				fmt.Println("sent")
+				results.SendResult(rr[fe.GetBib()])
+			}
 		case events.PlaceEventType:
-			places = append(places, event.(events.PlaceEvent))
+			pe := event.(events.PlaceEvent)
+
+			result := rr[pe.GetBib()]
+			result.Bib = pe.GetBib()
+			result.Athlete = athletes[pe.GetBib()]
+			result.Place = pe.GetPlace()
+			rr[pe.GetBib()] = result
+
+			if rr[pe.GetBib()].IsComplete() {
+				fmt.Println("sent")
+				results.SendResult(rr[pe.GetBib()])
+			}
 		}
 
 		event, err = inputEvents.GetRaceEvent()
 	}
-
-	for _, event := range places {
-		rr := RaceResult{event.GetBib(), athletes[event.GetBib()], event.GetPlace(), finishes[event.GetBib()].GetFinishTime().Sub(start[0].GetStartTime())}
-		if rr.IsComplete() {
-			results.SendResult(rr)
-			delete(finishes, event.GetBib())
-		} else {
-			incomplete = append(incomplete, event)
-		}
-	}
-	places = incomplete
-
 	return nil
 }

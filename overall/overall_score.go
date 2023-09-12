1d8b50394439db4b235e99bc69759f4c095d0f21
Matt Reynolds <32142979+MattReyn@users.noreply.github.com>
2023-09-12 12:26:20 -0400

    stubbing out overall score

--- overall/overall_score.go
+++ overall/overall_score.go
@@ -1,1 +1,14 @@
 package overall
+
+import (
+	"blreynolds4/event-race-timer/results"
+	"context"
+)
+
+type overallResult struct {
+	overallResults []results.RaceResult
+}
+
+func (OVR *overallResult) ScoreResults(context.Context, results.ResultSource) error {
+	return nil
+}

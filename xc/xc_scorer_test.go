5a486cb0f16a96bb5e43800c30f8847f0af0dd5b
Brian Reynolds <blreynolds4@gmail.com>
2023-09-12 12:19:49 -0400

    better xc result

--- xc/xc_scorer_test.go
+++ xc/xc_scorer_test.go
@@ -42,27 +42,27 @@ func TestXCMeetScore3Teams2Complete(t *testing.T) {
 	}
 	mock.Results = append(mock.Results, results.RaceResult{Bib: 17, Athlete: athletes[17], Place: 1, Time: durationHelper("23m29s")})
 	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[18], Place: 2, Time: durationHelper("24m11s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[1], Place: 3, Time: durationHelper("25m2s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[2], Place: 4, Time: durationHelper("25m10s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[19], Place: 5, Time: durationHelper("25m37s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[10], Place: 6, Time: durationHelper("26m40s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[2], Place: 7, Time: durationHelper("26m54s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[4], Place: 8, Time: durationHelper("26m57s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[5], Place: 9, Time: durationHelper("27m35s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[11], Place: 10, Time: durationHelper("27m45s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[6], Place: 11, Time: durationHelper("28m44s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[12], Place: 12, Time: durationHelper("28m44s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[13], Place: 13, Time: durationHelper("29m21s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[14], Place: 14, Time: durationHelper("30m40s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[15], Place: 15, Time: durationHelper("31m1s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[7], Place: 16, Time: durationHelper("31m35s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[20], Place: 17, Time: durationHelper("32m27s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[21], Place: 18, Time: durationHelper("34m6s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[22], Place: 19, Time: durationHelper("34m7s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[8], Place: 20, Time: durationHelper("36m22s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[16], Place: 21, Time: durationHelper("36m39s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[9], Place: 22, Time: durationHelper("37m25s")})
-	mock.Results = append(mock.Results, results.RaceResult{Bib: 18, Athlete: athletes[23], Place: 23, Time: durationHelper("37m46s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 1, Athlete: athletes[1], Place: 3, Time: durationHelper("25m2s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 2, Athlete: athletes[2], Place: 4, Time: durationHelper("25m10s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 19, Athlete: athletes[19], Place: 5, Time: durationHelper("25m37s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 10, Athlete: athletes[10], Place: 6, Time: durationHelper("26m40s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 2, Athlete: athletes[2], Place: 7, Time: durationHelper("26m54s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 4, Athlete: athletes[4], Place: 8, Time: durationHelper("26m57s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 5, Athlete: athletes[5], Place: 9, Time: durationHelper("27m35s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 11, Athlete: athletes[11], Place: 10, Time: durationHelper("27m45s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 6, Athlete: athletes[6], Place: 11, Time: durationHelper("28m44s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 12, Athlete: athletes[12], Place: 12, Time: durationHelper("28m44s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 13, Athlete: athletes[13], Place: 13, Time: durationHelper("29m21s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 14, Athlete: athletes[14], Place: 14, Time: durationHelper("30m40s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 15, Athlete: athletes[15], Place: 15, Time: durationHelper("31m1s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 7, Athlete: athletes[7], Place: 16, Time: durationHelper("31m35s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 20, Athlete: athletes[20], Place: 17, Time: durationHelper("32m27s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 21, Athlete: athletes[21], Place: 18, Time: durationHelper("34m6s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 22, Athlete: athletes[22], Place: 19, Time: durationHelper("34m7s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 8, Athlete: athletes[8], Place: 20, Time: durationHelper("36m22s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 16, Athlete: athletes[16], Place: 21, Time: durationHelper("36m39s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 9, Athlete: athletes[9], Place: 22, Time: durationHelper("37m25s")})
+	mock.Results = append(mock.Results, results.RaceResult{Bib: 23, Athlete: athletes[23], Place: 23, Time: durationHelper("37m46s")})
 
 	// expected scoring
 	// start with just the scores, fill the rest in
@@ -72,40 +72,40 @@ func TestXCMeetScore3Teams2Complete(t *testing.T) {
 			TeamScore: 28,
 			Finishers: []XCResult{
 				{
-					Athlete: athletes[1],
-					Score:   3,
+					mock.Results[2],
+					3,
 				},
 				{
-					Athlete: athletes[2],
-					Score:   4,
+					mock.Results[3],
+					4,
 				},
 				{
-					Athlete: athletes[3],
-					Score:   6,
+					mock.Results[6],
+					6,
 				},
 				{
-					Athlete: athletes[4],
-					Score:   7,
+					mock.Results[7],
+					7,
 				},
 				{
-					Athlete: athletes[5],
-					Score:   8,
+					mock.Results[8],
+					8,
 				},
 				{
-					Athlete: athletes[6],
-					Score:   10,
+					mock.Results[10],
+					10,
 				},
 				{
-					Athlete: athletes[7],
-					Score:   13,
+					mock.Results[15],
+					13,
 				},
 				{
-					Athlete: athletes[8],
-					Score:   0,
+					mock.Results[19],
+					0,
 				},
 				{
-					Athlete: athletes[9],
-					Score:   0,
+					mock.Results[21],
+					0,
 				},
 			},
 		},
@@ -114,44 +114,44 @@ func TestXCMeetScore3Teams2Complete(t *testing.T) {
 			TeamScore: 28,
 			Finishers: []XCResult{
 				{
-					Athlete: athletes[17],
-					Score:   1,
+					mock.Results[0],
+					1,
 				},
 				{
-					Athlete: athletes[18],
-					Score:   2,
+					mock.Results[1],
+					2,
 				},
 				{
-					Athlete: athletes[19],
-					Score:   5,
+					mock.Results[4],
+					5,
 				},
 				{
-					Athlete: athletes[11],
-					Score:   9,
+					mock.Results[9],
+					9,
 				},
 				{
-					Athlete: athletes[12],
-					Score:   11,
+					mock.Results[11],
+					11,
 				},
 				{
-					Athlete: athletes[13],
-					Score:   12,
+					mock.Results[12],
+					12,
 				},
 				{
-					Athlete: athletes[20],
-					Score:   14,
+					mock.Results[16],
+					14,
 				},
 				{
-					Athlete: athletes[21],
-					Score:   0,
+					mock.Results[17],
+					0,
 				},
 				{
-					Athlete: athletes[22],
-					Score:   0,
+					mock.Results[18],
+					0,
 				},
 				{
-					Athlete: athletes[23],
-					Score:   0,
+					mock.Results[22],
+					0,
 				},
 			},
 		},
